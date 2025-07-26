3252196b0fa0520ff8d7ad3059a6c5391fab0a86
ShwetaYadav224 <shweta22y@gmail.com>
2025-07-27 01:22:06 +0530

    backend

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -15,4 +15,41 @@ router.post("/test",async(req,res)=>{
       res.status(500).json({error:"failed to saved in DB"});
     }
 });
+router.get("/thread",async(req,res)=>{
+    try{
+     const threads= await Thread.find({}).sort({updatedAt:-1});
+     res.json(threads);
+    }catch(err){
+console.log(err);
+res.status(500).json({error: "Failed to fetch threads"});
+    }
+});
+router.get("/thread/:threadId", async(req,res)=>{
+const {threadId}=req.params;
+try{
+const thread=await Thread.findOne({threadId});
+if(!thread){
+    res.status(404).json({error:"Thread is not found"});
+}
+res.json(thread.messages);
+}catch(err){
+    console.log(err);
+    res.status(500).json({error: "Failed to fetch threads"});
+}
+
+});
+router.delete("/thread/:threadId", async(req,res)=>{
+    const {threadId}=req.params;
+    try{
+    const deletedThread=await Thread.findOneAndDelete({threadId});
+      if(!deletedThread){
+            res.status(404).json({error:"Thread not found"});
+      }else{
+        res.status(200).json({success:"Thread deleted successfully"});
+      }
+    }catch(err){
+     console.log(err);
+     res.status(500).json({error:"Can not delete thread"});
+    }
+})
 export default router;
